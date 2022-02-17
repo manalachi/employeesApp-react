81e2a8972073506be721652bd2bb53f9ccce7a30
manalachi <manalachi>
2022-02-17 23:48:12 +0200

    continue

---
+++ src/components/app/app.js
@@ -0,0 +1,25 @@
+import AppInfo from '../app-info';
+import SearchPanel from '../search-panel';
+import AppFilter from '../app-filter';
+import EmployeesList from '../employees-list';
+import EmployeesAddForm from '../employees-add-form';
+
+import './app.css';
+
+function App() {
+    return (
+        <div className="App">
+            <AppInfo/>
+
+            <div className="search-panel">
+                <SearchPanel/>  
+                <AppFilter/>              
+            </div>
+
+            <EmployeesList/>
+            <EmployeesAddForm/>
+        </div>
+    )
+}
+
+export default App;
